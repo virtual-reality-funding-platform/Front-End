254c964d795f808c4d40da79178df77383fac810
Greg Jordan <gregljordan@gmail.com>
2019-06-27 12:34:19 -0400

    Fixing styling issues

--- vrfund/src/components/auth/Login.js
+++ vrfund/src/components/auth/Login.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../../actions';
+import { 
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from 'semantic-ui-react';
 
 
 
@@ -15,13 +23,53 @@ export class Login extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h3>Login</h3>
-        <div>username <input type="text" ref={this.userRef} /></div>
-        <div>password <input type="password" ref={this.passRef} /></div>
 
-        <button onClick={this.onLogin}>Log in</button>
-      </div>
+      <Grid centered columns={2}>
+        <Grid.Column>
+            <Header as="h2" textAlign="center">
+                Login
+            </Header>
+            <Segment>
+                <Form size="large">
+                    <Form.Input
+                        fluid
+                        icon="user"
+                        iconPosition="left"
+                        placeholder="Username"
+                        type="text"
+                        ref={this.userRef}
+                    />
+                    <Form.Input
+                        fluid
+                        icon="lock"
+                        iconPosition="left"
+                        placeholder="Password"
+                        type="password"
+                        ref={this.passRef}
+                    />
+                    <Button color="green" fluid size="large" onClick={this.onLogin}>
+                        Login
+                    </Button>
+                </Form>
+            </Segment>
+            <Message>
+                Not registered yet? <a href="##">Sign Up</a>
+            </Message>
+        </Grid.Column>
+      </Grid>
+
+
+
+
+
+
+      // <div>
+      //   <h3>Login</h3>
+      //   <div>username <input type="text" ref={this.userRef} /></div>
+      //   <div>password <input type="password" ref={this.passRef} /></div>
+
+      //   <button onClick={this.onLogin}>Log in</button>
+      // </div>
     );
   }
 }
