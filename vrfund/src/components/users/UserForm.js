59298867beb823d648e8b98c42aeb0e6abb8c3ca
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-27 03:49:49 +0100

    create and add userform for registration on on nhomepage

--- vrfund/src/components/users/UserForm.js
+++ vrfund/src/components/users/UserForm.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import { addingUser } from "../../actions";
+export class UserForm extends Component {
+    nameRef = React.createRef()
+    aboutRef = React.createRef()
+    usernameRef = React.createRef()
+    passwordRef = React.createRef()
 
-export const UserForm = () => (
-        <div>
-            <h1>
-                This is the component that will be used to create the userForm, and will be imported into the UserContainer component.
-            </h1>
-        </div>
-    )
+    onAddUser = () => {
+      this.props.addingUser({
+        name: this.nameRef.current.value,
+        about: this.aboutRef.current.value,
+        username: this.usernameRef.current.value,
+        password: this.passwordRef.current.value,
+      });
+      this.nameRef.current.value = '';
+      this.aboutRef.current.value = '';
+      this.usernameRef.current.value = '';
+      this.passwordRef.current.value = '';
+    }
+
+    render() {
+        return (
+            <div>
+              <div><em>Name</em>
+              <input ref={this.nameRef} type="text" />
+              </div>
+
+              <div><em>About</em>
+              <input ref={this.aboutRef} type="text" />
+              </div>
+
+              <div><em>Username</em>
+              <input ref={this.usernameRef} type="text" />
+              </div>
+
+              <div><em>Password</em>
+              <input ref={this.passwordRef} type="text" />
+              </div>
+
+              <div>
+              <button onClick={this.onAddUser}>Add user</button>
+
+              </div>
+
+            </div>
+        )
+    }
+}
+
+export default connect(null, { addingUser})(UserForm);
 
