34c3b27219470ee1ed60ef06b64b5d01e1364df2
Greg Jordan <gregljordan@gmail.com>
2019-06-26 20:06:00 -0400

    Edited file

---
+++ vrfund/src/components/users/UserProjectForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserProjectForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
