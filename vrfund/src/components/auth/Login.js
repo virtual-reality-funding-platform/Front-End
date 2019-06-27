7a72101e2745593c0373e3f0a0ddc81e6c9430dd
Greg Jordan <gregljordan@gmail.com>
2019-06-27 00:24:06 -0400

    Made the password hidden on login

--- vrfund/src/components/auth/Login.js
+++ vrfund/src/components/auth/Login.js
@@ -18,7 +18,7 @@ export class Login extends React.Component {
       <div>
         <h3>Login</h3>
         <div>username <input type="text" ref={this.userRef} /></div>
-        <div>password <input type="text" ref={this.passRef} /></div>
+        <div>password <input type="password" ref={this.passRef} /></div>
 
         <button onClick={this.onLogin}>Log in</button>
       </div>
